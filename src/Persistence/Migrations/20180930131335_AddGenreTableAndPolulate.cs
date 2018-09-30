59aaa678f425ff0884e1b9bbb77ff44993f0d390
YIANNIS\Yiannis <yiannis.ioannidis@hotmail.gr>
2018-09-30 15:29:55 +0200

    more migration.

---
+++ src/Persistence/Migrations/20180930131335_AddGenreTableAndPolulate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Persistence.Entities;
+
+namespace Persistence.Migrations
+{
+    public partial class AddGenreTableAndPolulate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Create the lookup table
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    GenreName = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            // Populate the table with enum values
+            foreach (Genre genre in Enum.GetValues(typeof(Genre)))
+            {
+                migrationBuilder.Sql("INSERT INTO " +
+                    "Genre" +
+                    " SELECT " + (int)genre + ", '" + genre.ToString() + "'");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
