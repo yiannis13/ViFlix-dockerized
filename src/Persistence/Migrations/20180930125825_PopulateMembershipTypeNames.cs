7567ff8aea5b5056efdb488fecd77d6f93ce6259
YIANNIS\Yiannis <yiannis.ioannidis@hotmail.gr>
2018-09-30 15:12:37 +0200

    Add more migrations.

---
+++ src/Persistence/Migrations/20180930125825_PopulateMembershipTypeNames.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class PopulateMembershipTypeNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE dbo.MembershipTypes SET Name = 'Pay As You Go' where id=1");
+            migrationBuilder.Sql("UPDATE dbo.MembershipTypes SET Name = 'Monthly' where id=2");
+            migrationBuilder.Sql("UPDATE dbo.MembershipTypes SET Name = 'Quarterly' where id=3");
+            migrationBuilder.Sql("UPDATE dbo.MembershipTypes SET Name = 'Annual' where id=4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
