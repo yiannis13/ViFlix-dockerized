ffac07fc66b5fe00b0bc59ab0889d7d9ac88d16b
YIANNIS\Yiannis <yiannis.ioannidis@hotmail.gr>
2018-09-30 16:01:19 +0200

    more migrations.

---
+++ src/Persistence/Migrations/20180930133037_AddValidationToFields.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddValidationToFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Movies",
+                maxLength: 255,
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
